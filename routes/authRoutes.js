d8c172da01464464fe6ad24c2e6362b1f0c48e68
Ante Yoan <yoanante@mgil.com>
2019-03-28 17:48:47 -0400

    logout route and verify that login works with current_user route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,4 +9,17 @@ module.exports = (app) => {
   );
 
   app.get("/auth/google/callback", passport.authenticate("google"));
+
+  // logout is a function that is automagically attached to the req object.
+  // It takes the cookie and kills it and you are loggedout. 
+  app.get('/api/logout', (req, res)=> {
+    req.logout();
+    //proves that they are signed out as undefined or no content
+    res.send(req.user)
+  })
+
+  // user is automagically attached to the req object by passport. I love you passport.
+  app.get('/api/current_user', (req, res)=> {
+    res.send(req.user)
+  })
 };
