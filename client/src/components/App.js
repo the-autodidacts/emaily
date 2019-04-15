f4ebe3a0bbda0f6063b5cdc396d54ccbf19e8d28
Ante Yoan <yoanante@mgil.com>
2019-04-15 15:24:16 -0400

    Wired up react router top level routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
 
+//Your router stuff should never go in the index.js just here in the App component
+import { BrowserRouter, Route }  from 'react-router-dom';
+
+import Header from './Header'
+
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>Survey New</h2>;
+const Landing = () => <h2>Landing</h2>;
+
 const App = () => {
     return (
-        <div> 
-            Hi there!
-        </div>
-    )
+      <div>
+        <BrowserRouter>
+          <div>
+            <Header></Header>
+            <Route exact path="/" component={Landing}/>
+            <Route exact path="/surveys" component={Dashboard}/>
+            <Route exact path="/surveys/new" component={SurveyNew}/>
+            <Route />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDom from 'react-dom'
 // Start setup of Redux
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware }from 'redux'; // applyMiddleware will help with Thunk
+import { createStore, applyMiddleware } from 'redux'; // applyMiddleware will help with Thunk
+
+//import reducers
+import reducers from './reducers'
 
 
 import App from './components/App'
 
 // The first argument to createStore is a reducer the second argument is the initial state of the app
-const store = createStore(() => [], {}, applyMiddleware())
+const store = createStore(reducers, {}, applyMiddleware())
 
 ReactDom.render(
     <Provider store={store}><App></App></Provider>,
