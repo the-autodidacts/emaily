c723cff1757bc3010527068043bd52af28215889
Ante Yoan <yoanante@mgil.com>
2019-04-15 14:12:53 -0400

    initial setup of redux complete

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,16 @@
 import React from 'react';
 import ReactDom from 'react-dom'
+// Start setup of Redux
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware }from 'redux'; // applyMiddleware will help with Thunk
 
-ReactDom.render()
+
+import App from './components/App'
+
+// The first argument to createStore is a reducer the second argument is the initial state of the app
+const store = createStore(() => [], {}, applyMiddleware())
+
+ReactDom.render(
+    <Provider store={store}><App></App></Provider>,
+    document.querySelector("#root")
+);
