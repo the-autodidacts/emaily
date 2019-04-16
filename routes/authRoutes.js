1a3a3b29f84032f23f33a1f6df798ad530596132
Yoan Ante <yoanante@gmail.com>
2019-04-16 14:36:27 -0400

    handled broken /auth/google/callback route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,14 +8,19 @@ module.exports = (app) => {
     })
   );
 
-  app.get("/auth/google/callback", passport.authenticate("google"));
+  //passport.authenticate is a middleware. 
+  app.get(
+    "/auth/google/callback", 
+    passport.authenticate("google"), 
+    (req, res)=> {
+      res.redirect('/surveys')
+  });
 
   // logout is a function that is automagically attached to the req object.
   // It takes the cookie and kills it and you are loggedout. 
   app.get('/api/logout', (req, res)=> {
     req.logout();
-    //proves that they are signed out as undefined or no content
-    res.send(req.user)
+    res.redirect('/')
   })
 
   // user is automagically attached to the req object by passport. I love you passport.
