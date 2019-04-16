53eea24c106faa53f2d56ee0a8d329fb76872fea
Yoan Ante <yoanante@gmail.com>
2019-04-16 11:02:40 -0400

    cleaned up comments and refactored fetchUser to use async await

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,24 +1,23 @@
 import axios from 'axios';
 import { FETCH_USER } from './types'
 
-// Define our action creator
-const fetchUser = () => {
-    //we are using redux thunk here hence returning this function below which otherwise would not be needed what would be needed is the return an object with a type and a payload like so
-    /**
-        return {
-             type: FETCH_CURRENT_USER
-             payload: whatever it is. 
-        }
-           
-     */
+/* Define our action creator
+we are using redux thunk here hence returning this function below which otherwise would not be needed what would be needed is the return an object with a type and a payload like so
 
-     // The middleware Thunk looks out for a function instead of an object being returned and 
-     //automatically passes it a dispatch parameter which is another function. We want to dispatch that action after the async request has been completed.
-
-    return function (dispatch) {
-        axios.get("/api/current_user")
-            .then(res => {
-                dispatch({type: FETCH_USER, payload: res})
-            })
+    return {
+         type: FETCH_CURRENT_USER
+         payload: whatever it is. 
     }
-}
+       
+ 
+ The middleware Thunk looks out for a function instead of an object being returned and 
+ automatically passes it a dispatch parameter which is another function. We want to dispatch that action after the async request has been completed.
+*/
+ export const fetchUser = () => 
+     async dispatch => {
+        const res = await axios.get("/api/current_user")
+        dispatch({type: FETCH_USER, payload: res})
+        
+    }
+
+
