c2a330d5607aa3e6c7d233a57ea6122435cdefdb
Yoan Ante <yoanante@gmail.com>
2019-05-08 14:58:52 -0400

    Wired up test payment with stripe

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react' ;
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import Payments from './Payments'
 
 class Header extends Component {
 
@@ -16,14 +17,12 @@ class Header extends Component {
         );
       default: 
       return (
-        <>
-          <li>
-            <a href="/#">Add Credits</a>
-          </li>
-          <li>
+        [
+          <li key="1"><Payments/></li>,
+          <li key="2">
             <a href="/api/logout">Logout</a>
           </li>
-        </>
+        ]
       );
     }
   }
--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -3,14 +3,17 @@ import StripeCheckout from 'react-stripe-checkout';
 
 class Payments extends Component{
     render() {
-        debugger;
+
+
         return (
-            <StripeCheckout
-                amount={500}
-                token={token => console.log(token)}
-                stripeKey={process.env.REACT_APP_STRIPE_KEY}
-            />
-        )
+          <StripeCheckout
+            name="Emaily"
+            description="Buy 5 email credits for $5."
+            amount={500}
+            token={token => console.log(token)}
+            stripeKey={process.env.REACT_APP_STRIPE_KEY}
+          ><button className='btn'>Add Credits</button></StripeCheckout>
+        );
     }
 }
 
